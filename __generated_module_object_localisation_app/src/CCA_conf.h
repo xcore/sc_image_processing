08fc7856123e719ed8fd7e3f2cd31722f098dd1c
Sudha <sudha@xmos.com>
2013-07-05 10:30:35 +0530

    removed generated code

--- __generated_module_object_localisation_app/src/CCA_conf.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef CCA_CONF_H_
-#define CCA_CONF_H_
-
-#define OBJECT_LOCALISATION_CCA_MAX_LABEL	200		// Maximum number of labels
-
-#endif /* CCA_CONF_H_ */
--- __generated_module_object_localisation_app/src/binarisation_conf.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef BINARISATION_CONF_H_
-#define BINARISATION_CONF_H_
-
-#define OBJECT_LOCALISATION_BRIGHT_OBJ_DARK_BG	1	// 1 for bright objects on dark background; 0 otherwise
-
-#endif /* BINARISATION_CONF_H_ */
