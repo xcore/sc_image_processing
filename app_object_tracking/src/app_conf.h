55c4af2ed99c71f7c186777b075e3194dbbd07b8
sudhanatraj <sudha@xmos.com>
2014-06-06 15:33:12 +0530

    restructured

---
+++ app_object_tracking/src/app_conf.h
@@ -0,0 +1,10 @@
+
+#ifndef APP_CONF_H_
+#define APP_CONF_H_
+
+// Bright objects on a dark background will be detected
+
+#define SIZE_THRESHOLD 50	// For removing unwanted connected components
+#define BOX_COLOUR 0x07E0	// RGB565 for Green colour
+
+#endif /* APP_CONF_H_ */
